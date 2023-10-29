34292435dabfa799f03085d8e9dda08f67929358
ihgs <hgs.dev@gmail.com>
2023-10-29 13:08:47 +0900

    pwa化

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
+import {VitePWA} from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      includeAssets:[
+        'offline.html',
+        'favicon.svg',
+        'favicon.ico',
+        'rebots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        "theme_color": "#f69435",
+        "background_color": "#f69435",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "name": "myRecords",
+        "short_name": "myRecords",
+        "icons": [
+            {
+                "src": "/icon-192x192.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "/icon-256x256.png",
+                "sizes": "256x256",
+                "type": "image/png"
+            },
+            {
+                "src": "/icon-384x384.png",
+                "sizes": "384x384",
+                "type": "image/png"
+            },
+            {
+                "src": "/icon-512x512.png",
+                "sizes": "512x512",
+                "type": "image/png"
+            }
+        ]
+      }
+    })
+  ],
 })
