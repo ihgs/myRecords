b34b882e29b693ec3206f444632b9ed474c77a89
ihgs <hgs.dev@gmail.com>
2023-10-29 16:15:26 +0900

    update icon

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+    ? "/myRecords/"
+    : "./",
   plugins: [
     react(),
     VitePWA({
@@ -19,28 +22,28 @@ export default defineConfig({
         "theme_color": "#f69435",
         "background_color": "#f69435",
         "display": "standalone",
-        "scope": "/",
-        "start_url": "/",
+        "scope": "https://ihgs.github.io/myRecords/",
+        "start_url": ".",
         "name": "myRecords",
         "short_name": "myRecords",
         "icons": [
             {
-                "src": "/icon-192x192.png",
+                "src": "/myRecords/icon-192x192.png",
                 "sizes": "192x192",
                 "type": "image/png"
             },
             {
-                "src": "/icon-256x256.png",
+                "src": "/myRecords/icon-256x256.png",
                 "sizes": "256x256",
                 "type": "image/png"
             },
             {
-                "src": "/icon-384x384.png",
+                "src": "/myRecords/icon-384x384.png",
                 "sizes": "384x384",
                 "type": "image/png"
             },
             {
-                "src": "/icon-512x512.png",
+                "src": "/myRecords/icon-512x512.png",
                 "sizes": "512x512",
                 "type": "image/png"
             }
